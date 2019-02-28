504221db7cf9813ccd8ddad5fe85a98dfaa03efa
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-28 00:07:08 -0800

    Add oidc-provider and tidy server js oidc-provider allows for creating cusotm server for openid connect implementation

--- src/central-server/index.js
+++ src/central-server/index.js
@@ -1,15 +1,15 @@
 let express = require("express");
 let opn = require("opn");
 let path = require("path");
-
 let app = express();
 
+// Routes
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.use(express.static("public"));
-
+// Listening
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Working on port ${port}`);
