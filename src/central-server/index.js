3c7b33b680edf369045f5c4824e469b9ca11de7f
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-02-28 00:13:44 -0800

    Create authentication / authorization code

--- src/central-server/index.js
+++ src/central-server/index.js
@@ -1,7 +1,40 @@
 let express = require("express");
 let opn = require("opn");
+const Provider = require('oidc-provider');
 let path = require("path");
 let app = express();
+
+// Auth
+const configuration = {
+  // ... see available options /docs/configuration.md
+};
+const clients = [{
+  client_id: 'foo',
+  client_secret: 'bar',
+  redirect_uris: ['http://lvh.me:8080/cb'],
+  // + other client properties
+}];
+
+const oidc = new Provider('http://localhost:4001', configuration);
+
+let server;
+(async () => {
+  await oidc.initialize({ clients });
+  // express/nodejs style application callback (req, res, next) for use with express apps, see /examples/express.js
+  oidc.callback;
+
+  // koa application for use with koa apps, see /examples/koa.js
+  oidc.app;
+
+  // or just expose a server standalone, see /examples/standalone.js
+  server = oidc.listen(3000, () => {
+    console.log('oidc-provider listening on port 3000, check http://localhost:3000/.well-known/openid-configuration');
+  });
+})().catch((err) => {
+  if (server && server.listening) server.close();
+  console.error(err);
+  process.exitCode = 1;
+});
 
 // Routes
 app.use(express.static("public"));
