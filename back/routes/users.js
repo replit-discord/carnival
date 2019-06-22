9abcb3927b1b6b472635e8828e862a34952cb7d9
TheDrone7 <hsbhatia765@gmail.com>
2019-06-22 22:42:54 +0530

    added  route

--- back/routes/users.js
+++ back/routes/users.js
@@ -13,7 +13,44 @@ dbPool.connect().catch(e => {
   process.exit(5);
 });
 
-/* GET users listing. */
+const authorize = (req, res, next) => {
+  if (req.headers.authorization && req.body['username']) {
+    if (req.headers.authorization.split(' ').shift() == 'Bearer') {
+      dbPool
+        .query(
+          `SELECT user_name FROM users WHERE secret_id='${
+            jwt.verify(req.headers.authorization.split(' ').pop(), jwtKey)[
+              'secret_id'
+            ]
+          }';`
+        )
+        .then(result => {
+          if (result.rowCount > 0) {
+            if (result.rows[0]['user_name'] == req.body['username']) {
+              next();
+            } else {
+              res.status(404).json({ error: 'Invalid credentials provided.' });
+            }
+          } else {
+            res.status(404).json({ error: 'Invalid token was passed.' });
+          }
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err.message,
+            code: err.code
+          });
+        });
+    } else {
+      res.status(400).json({ error: 'Invalid token type.' });
+    }
+  } else {
+    res
+      .status(400)
+      .json({ error: 'Authentication token or username not provided.' });
+  }
+};
+
 router.get('/is-authorized', function(req, res, next) {
   if (req.cookies['token']) {
     dbPool
@@ -38,6 +75,42 @@ router.get('/is-authorized', function(req, res, next) {
   } else {
     res.sendStatus(401);
   }
+});
+
+router.post('/update-data', authorize, async (req, res) => {
+  if (req.body['userEmail'])
+    await dbPool
+      .query(
+        `UPDATE users SET user_email='${
+          req.body['userEmail']
+        }' WHERE user_name='${req.body['username']}';`
+      )
+      .catch(err => {
+        res.status(500).json({
+          error: err.message,
+          code: err.code
+        });
+      });
+
+  if (req.body['userPreferences']) {
+    console.log(JSON.stringify(req.body['userPreferences']));
+    await dbPool
+      .query(
+        `UPDATE users SET user_preferences='${JSON.stringify(
+          req.body['userPreferences']
+        )}' WHERE user_name='${JSON.stringify(req.body['username'])}';`
+      )
+      .catch(err => {
+        res.status(500).json({
+          error: err.message,
+          code: err.code
+        });
+      });
+  }
+
+  res.status(200).json({
+    success: true
+  });
 });
 
 router.get('/check/:username', function(req, res) {
