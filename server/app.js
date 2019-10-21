06ff9273a43944e9d23bcef1cecccdfc9e34b837
eankeen <24364012+eankeen@users.noreply.github.com>
2019-10-21 03:01:45 -0700

    fix: remove commented out vital functionality

--- server/app.js
+++ server/app.js
@@ -2,10 +2,10 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser'
 const logger = require('morgan')
-// import { default as logger } from 'morgan';
+import { default as logger } from 'morgan';
 
 import indexRouter from './routes/index';
-// import usersRouter from './routes/users';
+import usersRouter from './routes/users';
 
 const app = express();
 
@@ -13,9 +13,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 export default app;
