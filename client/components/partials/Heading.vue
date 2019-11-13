8348be2a007d79811263af2a8edc1a85a3bee12a
eankeen <24364012+eankeen@users.noreply.github.com>
2019-11-12 17:02:07 -0800

    fix: backspace not showing up on turn

--- client/components/partials/Heading.vue
+++ client/components/partials/Heading.vue
@@ -1,10 +1,10 @@
 <template>
-  <div ref="headerWrapperWrapper" class="header-wrapper-wrapper">
+  <div class="header-wrapper-wrapper" @click="enlargeHeader">
     <div class="header-wrapper">
-      <header class="header" @click="enlargeHeader">
-        <h1>content</h1>
+      <header class="header">
+        <h1>Carnival</h1>
       </header>
-      <header class="header-back">
+      <header class="navigation">
         <h1>back</h1>
       </header>
     </div>
@@ -33,19 +33,18 @@ export default {
       anime({
         targets: '.header-wrapper-wrapper',
         translateX: 0,
-        translateY: 0
+        translateY: 0,
+        easing: 'spring(1, 100, 90, 10)'
       });
 
       anime({
         targets: '.header-wrapper',
         translateX: margin,
         translateY: margin,
-        rotateY: '-180deg',
-        easing: 'spring(1, 100, 90, 10)',
-        // easing: 'easeOutExpo',
+        rotateY: '180deg',
         width: finalWidth,
         height: finalHeight,
-        opacity: 0.5
+        easing: 'spring(1, 100, 90, 3)'
       });
     }
   }
@@ -55,45 +54,72 @@ export default {
 <style lang="postcss" scoped>
 .header-wrapper-wrapper {
   position: fixed;
-  transform: translate(10px, 10px);
-  perspective: 5000px;
   z-index: 1000;
+  perspective: 5000px;
+
+  --border-radius: 5px;
+  border-radius: var(--border-radius);
+
+  transform: translate(10px, 10px);
+  transition: all 0.25s ease-out-quint;
+}
+
+.header-wrapper-wrapper:hover {
+  transform: translate(10px, 10px) scale(1.03, 1.03);
+  transition: all 0.25s ease-out-quint;
+}
+
+.header-wrapper-wrapper:active {
+  transform: translate(10px, 10px) scale(0.97, 0.97);
+  transition: all 0.25s ease-out-quint;
 }
 
 .header-wrapper {
   position: relative;
-
-  width: 170px;
+  width: 190px;
   height: 60px;
+
+  transform-style: preserve-3d;
+
+  box-shadow: 2px 4px 4px $oc-gray-4;
+  border-radius: var(--border-radius);
+}
+
+.header,
+.navigation {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  backface-visibility: hidden;
+  border-radius: var(--border-radius);
 }
 
 .header {
-  position: absolute;
-  top: 0px;
-  right: 0px;
-  bottom: 0px;
-  left: 0px;
-
-  background-color: $bg;
-  box-shadow: 2px 4px 4px $oc-gray-4;
-  border-radius: 5px;
-  cursor: pointer;
-  z-index: 60;
-}
-
-.header-back {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  bottom: 10px;
-  left: 10px;
-  z-index: 50;
-}
-
-.menu {
   display: flex;
   align-items: center;
   justify-content: center;
+  background-color: $bg;
+  cursor: pointer;
+}
+
+.header::before {
+  content: '😏';
+  font-size: 2rem;
+  margin-right: 5px;
+}
+
+.navigation {
+  display: grid;
+  background-color: $bg;
+  transform: rotateY(180deg);
+}
+
+.navigation h1 {
+  margin: auto;
+  text-align: center;
 }
 
 h1 {
