a951c0b83595bb37d07d02621f2b18977ea6fd5c
TheDrone7 <hsbhatia765@gmail.com>
2020-03-31 21:35:54 +0530

    fixed config

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
-let discoId = '694174608156262431';
-let discoSecret = 'DK8bVTvB_NNaobMHsrgrqoOIvHNePbJQ';
-let jwtKey = 'mySuperSecretKey';
+let discoId = process.env.discoId;
+let discoSecret = process.env.discoSecret;
+let jwtKey = process.env.jwtKey;
 
 export { discoId, discoSecret, jwtKey };
